e5196b163d70ffd631889bde7205b674529e252f
Alex Bond <fantom4igg@gmail.com>
2019-04-25 13:30:10 +0300

    addaded digger.sin and new class Terrain

--- Inheritance/digger.csproj/DiggerTask.cs
+++ Inheritance/digger.csproj/DiggerTask.cs
@@ -8,5 +8,50 @@ namespace Digger
 {
     //Напишите здесь классы Player, Terrain и другие.
 
-   
+
+    class Player : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            return true;
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Terrain : ICreature
+    {
+        public CreatureCommand Act(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeadInConflict(ICreature conflictedObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetDrawingPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetImageFileName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
